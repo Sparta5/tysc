8f18c8cb0d1359d656bc5475e4b615154151be5e
bing <bing@123.com>
2020-10-21 02:02:40 +0800

    10/21am

---
+++ webShopping/src/components/myItem/mySearch.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="mysearch">
+    <div class="list">
+      <span class="iconfont icon-caidan"></span>
+      <mt-search class="search" v-model="value"
+               cancel-text="" placeholder="搜索商品..."
+                :result.sync="result">
+           <!-- <mt-cell>
+           </mt-cell> -->
+      </mt-search>
+      <span class="iconfont icon-lingdang"></span>
+    </div>
+  </div>
+</template>
+<style lang="scss">
+.mysearch{
+  width: 100%;
+  display: block;
+  margin: 0px auto;
+  .search{//  搜索样式设置
+     height: 2.75rem;width: 85%;
+     height: 44px;
+    .mint-searchbar{
+      background-color:white;
+      // left: 10%;
+      .mint-searchbar-inner{
+        background-color: #f2f2f2;
+        border-radius: 1.125rem;
+        i{font-size: 1rem;margin: 0px 0.3125rem;}
+      }
+      .mint-searchbar-core{background-color: #f2f2f2;}
+    }
+    // .mint-search-list{width: 640px; margin: 0px auto;}
+  }
+  // 菜单样式和右边
+  .list{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    >.iconfont{
+      width: 28px;height:28px;
+      color: #666;
+      font:400 28px 微软雅黑;
+    }
+  }
+}
+</style>
--- webShopping/src/views/About.vue
+++ webShopping/src/views/About.vue
@@ -1,5 +1,25 @@
 <template>
   <div class="about">
+    <my-search></my-search>
     <h1>This is an about page</h1>
   </div>
 </template>
+<script>
+import mySearch from '../components/myItem/mySearch'
+export default {
+  components:{
+    mySearch
+  }
+}
+</script>
+<style lang="scss">
+body{max-width: 650px;margin: 0px auto;}
+.about{
+  height: 300px;margin: 0px auto;
+  // border: 1px solid red;
+  h1{
+    width: 95%;height: 2.25rem; border: 1px solid rgb(160, 137, 5);
+    margin-top: 6px;  
+  }
+}
+</style>

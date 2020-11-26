ecadc0a7c052abc22f7b4a3d6f62d7ae2e4b055a
bing <bing@123.com>
2020-11-26 23:40:45 +0800

    11/26feat

--- Z_nuxt/nuxtItem/pages/login.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <div class="login">
-    <h3>登录</h3>
-  </div>
-</template>
--- Z_nuxt/nuxtItem/store/index.js
+++
@@ -1,7 +0,0 @@
-export const actions = {
-  nuxtServerInit(store,context){//初始化钩子
-    //初始化store当中
-    // console.log('nuxtServerInit',store,context)
-    console.log('nuxtServerInit')
-  }
-}
